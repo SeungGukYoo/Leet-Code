9706baa4b59dc60c0f831ee7a6e08eac7bc344e0
SeungGukYoo <dbtmd878@gmail.com>
2023-10-24 15:39:54 +0900

    36. Valid Sudoku

--- 36. Valid Sudoku/index.js
+++ 36. Valid Sudoku/index.js
@@ -1,21 +1,29 @@
-function solution(board) {
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+  const setSdoku = Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => new Set()));
+
   for (let i = 0; i < 9; i++) {
+    let rowSet = new Set();
+    let colSet = new Set();
     for (let j = 0; j < 9; j++) {
-      console.log(board[3 * Math.floor(i / 3) + Math.floor(j / 3)][((3 * i) % 3) + (j % 3)]);
-      // board[3 * Math.floor(i / 3) + Math.floor(j / 3)][((3 * i) % 3) + (j % 3)];
+      let colPos = parseInt(i / 3);
+      let rowPos = parseInt(j / 3);
+      if (board[i][j] !== "." && rowSet.has(board[i][j])) {
+        return false;
+      }
+      if (board[j][i] !== "." && colSet.has(board[j][i])) {
+        return false;
+      }
+      if (board[i][j] !== "." && setSdoku[colPos][rowPos].has(board[i][j])) {
+        return false;
+      }
+      setSdoku[colPos][rowPos].add(board[i][j]);
+      rowSet.add(board[i][j]);
+      colSet.add(board[j][i]);
     }
   }
-}
-
-const board = [
-  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
-];
-solution(board);
+  return true;
+};
